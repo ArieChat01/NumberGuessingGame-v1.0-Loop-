bdf031f937bd89afc2746255a3bced911cd963cc
Unknown <arieauna.chatmon.2019@student.lhschools.org>
2018-09-21 14:21:37 -0400

    Adding working loop, but not equals.

--- src/NumberGuesser.java
+++ src/NumberGuesser.java
@@ -15,17 +15,33 @@ public class NumberGuesser {
 
         System.out.println("Welcome to the NumberGuessing Game!");
         System.out.println("Here's how to play. I am going to think of random number\nbetween 1 and 100.");
-        System.out.println("It is your job, to try to guess that number.\nYou will have 6 opportunities.\n \nAre you ready?");
+        System.out.println("It is your job, to try to guess that number.\nYou will have 5 opportunities.\n \nAre you ready?");
         System.out.print("What number am I thinking of?");
         System.out.println();
+        int i = 5;
 
         userNumber = keyboard.nextInt();
 
-        while(pickedNumber>userNumber || pickedNumber<userNumber){
-            System.out.print("Nope. That answer was too low.\nGuess again.\n");
-            userNumber=keyboard.nextInt();
+        while (pickedNumber > userNumber || pickedNumber < userNumber || userNumber == pickedNumber) {
+            if (i == 1){
+                System.out.print("You have run out of tries.");
+                System.exit(0);
             }
-        System.out.println("That it you got it right!");
+
+            else if (pickedNumber > userNumber) {
+                System.out.print("Nope. That answer was too low.\nGuess again.\n");
+                i--;
+                System.out.println(i + " :tries left");
+                userNumber = keyboard.nextInt();
+
+            }
+            else if (pickedNumber < userNumber){
+                System.out.println("Nope. That answer was too high.\nGuess again.");
+                i--;
+                System.out.println(i + " :tries left.");
+                userNumber = keyboard.nextInt();
+            }
+
+        }
     }
 }
-
