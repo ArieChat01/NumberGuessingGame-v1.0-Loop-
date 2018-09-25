2045f45c86fb6d9bc68f889d6300a79f79592351
Unknown <arieauna.chatmon.2019@student.lhschools.org>
2018-09-25 14:05:51 -0400

    Finalized loop settings with different equals equation.

--- src/NumberGuesser.java
+++ src/NumberGuesser.java
@@ -10,15 +10,15 @@ public class NumberGuesser {
         String userPlayAgain;
         boolean notWonGame;
         boolean playAgain;
+        String emptySpace;
 
         pickedNumber = (int) (Math.random() * 100);
         keyboard = new Scanner(System.in);
         notWonGame = true;
         playAgain = true;
 
-        System.out.print(pickedNumber + "\n");
-
         while (playAgain) {
+
         System.out.println("Welcome to the NumberGuessing Game!");
         System.out.println("Here's how to play. I am going to think of random number\nbetween 1 and 100.");
         System.out.println("It is your job, to try to guess that number.\nYou will have 5 opportunities.\n \nAre you ready?");
@@ -50,16 +50,17 @@ public class NumberGuesser {
                     notWonGame = false;
                 }
             }
-            System.out.println("\nWant to play again?\n(Answer \"yes\" or \"no\")");
+            System.out.println("\nWant to play again?(Answer \"yes\" or \"no\")");
+            emptySpace = keyboard.nextLine();
             userPlayAgain = keyboard.nextLine();
-            userPlayAgain = keyboard.nextLine();
-            if (userPlayAgain == "yes"){
-                System.out.println("");
+            if (userPlayAgain.equals("yes")){
+             playAgain = true;
             }
-            else if (userPlayAgain == "no"){
+            else if (!userPlayAgain.equals("yes")){
+                System.out.println("Thanks for Playing!");
                 playAgain = false;
             }
-            else System.out.println("Thanks for Playing!");
+
         }
     }
 }
